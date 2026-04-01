adc7ff3e5f9e26bb64e24506e42464c155fd4394
Michael <michaelkmwaura@gmail.com>
2026-04-01 17:36:37 -0400

    updated the simulation

--- server/firebase.js
+++ server/firebase.js
@@ -1,11 +1,25 @@
+const fs = require('fs');
 const path = require('path');
 const admin = require('firebase-admin');
 
-const serviceAccount = require(path.join(__dirname, '..', 'serviceAccountKey.json'));
+const DATABASE_URL = 'https://iot-project-36aef-default-rtdb.firebaseio.com';
+
+function loadServiceAccount() {
+  const keyPath =
+    process.env.GOOGLE_APPLICATION_CREDENTIALS ||
+    process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+  if (!keyPath) {
+    throw new Error(
+      'Set GOOGLE_APPLICATION_CREDENTIALS (or FIREBASE_SERVICE_ACCOUNT_PATH) to the absolute path of your Firebase service account JSON. Do not commit that file.'
+    );
+  }
+  const resolved = path.resolve(keyPath);
+  return JSON.parse(fs.readFileSync(resolved, 'utf8'));
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://iot-project-36aef-default-rtdb.firebaseio.com',
+  credential: admin.credential.cert(loadServiceAccount()),
+  databaseURL: DATABASE_URL,
 });
 
 const db = admin.database();
